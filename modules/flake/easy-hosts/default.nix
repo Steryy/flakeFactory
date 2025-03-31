7a41bc6eabff94b0b9ba7e5eb2df2470e33c264d
steryy <contact@steryy.xyz>
2025-03-31 22:51:03 +0200

    feat: added templated disko config

--- modules/flake/easy-hosts/default.nix
+++ modules/flake/easy-hosts/default.nix
@@ -1,6 +1,31 @@
-{inputs, ...}: let
-in {
+{
+  inputs,
+  config,
+  flakeRoot,
+  ...
+}: {
   imports = [
     inputs.easy-hosts.flakeModule
   ];
+  easy-hosts = {
+    shared.modules = [
+      {
+      }
+      ({
+        options,
+        lib,
+        ...
+      }: {
+        config = lib.optionalAttrs (options ? "home-manager") {
+          home-manager.extraSpecialArgs = {
+            inherit flakeRoot inputs;
+          };
+        };
+      })
+    ];
+    shared.specialArgs = {
+      inherit inputs;
+      inherit flakeRoot;
+    };
+  };
 }
---
+++ modules/flake/easy-hosts/disko.nix
@@ -0,0 +1,115 @@
+{
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  nixpkgs = inputs.nixpkgs;
+  diskoImport = file: let
+    enume =
+      lib.collect (x: lib.isString x)
+      (lib.mapAttrsRecursive
+        (p: _: builtins.concatStringsSep "." p)
+        config.haumea.diskoModules);
+    evaled =
+      (
+        lib.evalModules
+        {
+          specialArgs =
+            config.easy-hosts.shared.specialArgs;
+          modules = [
+            file
+            {
+              options.templates = lib.mkOption {
+                default = [];
+                type = lib.types.listOf (
+                  lib.types.submodule {
+                    options = {
+                      config = lib.mkOption {
+                        type = lib.types.raw;
+                        default = {};
+                      };
+                      name = lib.mkOption {
+                        type =
+                          lib.types.nullOr
+                          (lib.types.enum
+                            enume);
+                        default = null;
+                      };
+                    };
+                  }
+                );
+              };
+              options.disko = lib.mkOption {
+                type = lib.types.raw;
+              };
+            }
+          ];
+        }
+      )
+      .config;
+  in
+    if evaled.templates == []
+    then {
+      imports = [
+        evaled.disko
+      ];
+    }
+    else {
+      imports = (
+        map (
+          x: {
+            imports = [
+              x.config
+              (lib.getAttrFromPath (lib.splitString "." x.name) config.haumea.diskoModules)
+            ];
+          }
+        )
+        evaled.templates
+      );
+    };
+  removeDash = attr:
+    lib.mapAttrs (
+      _: v:
+        if builtins.isAttrs v
+        then removeDash v
+        else v
+    )
+    (lib.filterAttrs (n: _: !lib.hasPrefix "_" n) attr);
+in {
+  config = {
+    flake. diskoConfigurations = lib.pipe config.easy-hosts.hostsBare [
+      (lib.filterAttrs (_: x: lib.pathExists "${x._path}/disko.nix"))
+      (lib.mapAttrs (
+        v: x: let
+          cfg =
+            config.flake.nixosConfigurations."${v}".config;
+        in {
+          disko.devices.disk =
+            lib.mapAttrs (_: v: {
+              inherit (v) device;
+              content =
+                removeDash v.content;
+              # inherit (v) content;
+            })
+            cfg.disko.devices.disk;
+        }
+      ))
+    ];
+    easy-hosts.functionsList = [
+      (
+        x: let
+          disko = "${x._path}/disko.nix";
+        in {
+          modules =
+            if lib.pathExists disko
+            then [
+              inputs.disko.nixosModules.disko
+              (diskoImport disko)
+            ]
+            else [];
+        }
+      )
+    ];
+  };
+}
