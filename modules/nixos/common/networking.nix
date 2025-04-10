04fe41d9037bd2a41ab58161d4af4b567a9f4800
steryy <contact@steryy.xyz>
2025-04-10 19:04:44 +0200

    feat: add additional options for networking

---
+++ modules/nixos/common/networking.nix
@@ -0,0 +1,35 @@
+{ lib, config, ... }: {
+
+  options = {
+    networking = {
+      domains = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+      };
+      exposedServices = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submodule {
+          options = {
+            port = lib.mkOption {
+              default = null;
+              type = lib.types.nullOr (lib.types.int);
+            };
+
+            host = lib.mkOption {
+              default = "localhost";
+              type = lib.types.str;
+            };
+          };
+        });
+      };
+    };
+  };
+
+  config = {
+    networking = {
+      firewall.allowedTCPPorts = builtins.filter (x: x != null)
+        (map (x: if x.host == "localhost" then x.port else null)
+          (builtins.attrValues config.networking.exposedServices));
+    };
+  };
+
+}
