194275480be739b8352d8d38f8869313f59947c8
steryy <contact@steryy.xyz>
2025-04-12 19:10:58 +0200

    refactor: move importer to lib

---
+++ lib/importer.nix
@@ -0,0 +1,55 @@
+{ lib, ... }: rec {
+
+  importer = nixModules:
+    lib.mapAttrsRecursive (path: v:
+      lib.mkOption {
+        default = { };
+        type = lib.types.submodule ({ config, ... }: {
+          options = {
+            enable = lib.mkEnableOption
+              ("Enable " + (lib.strings.concatStringsSep " " path));
+            path = lib.mkOption {
+              type = lib.types.nullOr lib.types.path;
+              readOnly = true;
+              default = if config.enable then v else null;
+
+            };
+
+          };
+        });
+      }) nixModules;
+
+  defaultImport = nixModules: class:
+    lib.mapAttrsRecursive (path: _:
+      if lib.lists.elemAt path 1 == class then {
+        enable = lib.mkDefault true;
+      } else
+        { }) { importer = nixModules; };
+  eval = { class ? "", modules ? [ ], specialArgs ? { }, importerModules ? { }
+    , ... }:
+    let
+      modulesToAdd = [
+        { options = { importer = importer importerModules; }; }
+        (defaultImport importerModules class)
+        (defaultImport importerModules "common")
+      ];
+      evaled = lib.evalModules {
+
+        inherit specialArgs;
+        modules =
+
+          modules ++ modulesToAdd ++ [{
+            config._module.check = true;
+            config._module.freeformType = lib.types.unspecified;
+
+          }];
+      };
+    in {
+      modules = modulesToAdd ++ [{
+        imports = map (x: x.path) (lib.collect
+          (x: x ? "path" && x ? "enable" && x.enable && lib.isPath x.path)
+          evaled.config.importer);
+      }];
+    };
+
+}
--- modules/nixos/inputs/homeImport.nix
+++ modules/nixos/inputs/homeImport.nix
@@ -3,55 +3,9 @@ let
   hn = config.networking.hostName;
   dir = flakeRoot + "/homes";
 
-  importer = lib.mapAttrsRecursive (path: v:
-    lib.mkOption {
-      default = { };
-      type = lib.types.submodule ({ config, ... }: {
-        options = {
-          enable = lib.mkEnableOption
-            ("Enable " + (lib.strings.concatStringsSep " " path));
-          path = lib.mkOption {
-            type = lib.types.nullOr lib.types.path;
-            readOnly = true;
-            default = if config.enable then v else null;
-
-          };
-
-        };
-      });
-    }) homeModules;
-
-  defaultImport = class:
-    lib.mapAttrsRecursive (path: _:
-      if lib.lists.elemAt path 1 == class then {
-        enable = lib.mkDefault true;
-      } else
-        { }) { importer = homeModules; };
   eval = x:
-    let
-      modulesToAdd = [
-        { options = { inherit importer; }; }
-        (defaultImport (x.class or ""))
-        (defaultImport "common")
-      ];
-      evaled = lib.evalModules {
-
-        specialArgs = x.specialArgs or { };
-        modules =
-
-          (x.modules or [ ]) ++ modulesToAdd ++ [{
-            config._module.check = true;
-            config._module.freeformType = lib.types.unspecified;
-
-          }];
-      };
-    in {
-      modules = modulesToAdd ++ [{
-        imports = map (x: x.path) (lib.collect
-          (x: x ? "path" && x ? "enable" && x.enable && lib.isPath x.path)
-          evaled.config.importer);
-      }];
-    };
+    (import (flakeRoot + "/lib/importer.nix") { inherit lib; }).eval
+    (x // { importerModules = homeModules; });
 
   users = lib.pipe dir [
     builtins.readDir
