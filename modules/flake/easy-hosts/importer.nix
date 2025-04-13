bd339bcf5692ffa1b1ab4e386d97adfd351d9a22
steryy <contact@steryy.xyz>
2025-04-13 19:34:53 +0200

    feat: load facter if exists

--- modules/flake/easy-hosts/importer.nix
+++ modules/flake/easy-hosts/importer.nix
@@ -1,8 +1,26 @@
-{ lib, config, flakeRoot, ... }:
+{ lib, config, flakeRoot, inputs, ... }:
 let
   nixModules = config.haumea.nixModules;
   eval = x:
     (import (flakeRoot + "/lib/importer.nix") { inherit lib; }).eval
     (x // { importerModules = nixModules; });
 
-in { config = { easy-hosts = { functionsList = [ (x: eval x) ]; }; }; }
+in {
+  config = {
+    easy-hosts = {
+      functionsList = [
+        (x: eval x)
+        (x:
+          let facter = flakeRoot + "/vars/${x._hostName}/facter.json";
+          in {
+            modules = if lib.pathExists facter then [
+              inputs.nixos-facter-modules.nixosModules.facter
+              { config.facter.reportPath = facter; }
+            ] else
+              [ ];
+
+          })
+      ];
+    };
+  };
+}
