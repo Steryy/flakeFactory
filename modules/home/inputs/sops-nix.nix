5ff301b2cedbc8389fb5453d84ea7e2d313d5b5b
steryy <contact@steryy.xyz>
2025-04-17 19:46:47 +0000

    feat: add inputs sops-nix

---
+++ modules/home/inputs/sops-nix.nix
@@ -0,0 +1,18 @@
+{ inputs, config, flakeRoot, lib, osConfig, ... }:
+
+let
+  hostname = osConfig.networking.hostName;
+  userName = config.home.username;
+in {
+  options.sops.secrets = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule ({ name, ... }: {
+      config = {
+        format = "binary";
+        sopsFile = flakeRoot
+          + "/vars/per-machines/${hostname}/per-user/${userName}/${name}/secret";
+      };
+    }));
+  };
+
+  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+}
