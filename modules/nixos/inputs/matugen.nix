1ec2957805350cb83c473d3f3d7de961bac4e96e
steryy <contact@steryy.xyz>
2025-04-17 11:45:35 +0000

    perf: matugen use pkgs

--- modules/nixos/inputs/matugen.nix
+++ modules/nixos/inputs/matugen.nix
@@ -1,10 +1,9 @@
-{inputs, ...}: {
-  imports = [
-    inputs.matugen.nixosModules.matugen
-  ];
+{ inputs, pkgs, ... }: {
+  imports = [ inputs.matugen.nixosModules.matugen ];
 
   programs.matugen = {
     enable = true;
     jsonFormat = "rgb";
+    package = with pkgs; matugen;
   };
 }
