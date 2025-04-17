7a5770c5b9027b6214948def1e47b4d1f1bff21f
steryy <contact@steryy.xyz>
2025-04-17 11:05:55 +0000

    fix: hosts waifu/holo

--- hosts/waifus/holo/default.nix
+++ hosts/waifus/holo/default.nix
@@ -1,8 +1,13 @@
-{
-  specialArgs = { };
+let adminUser = "myuri";
+in {
+  deploy = { inherit adminUser; };
   modules = [
-    ({ inputs, ... }: {
+    ./disko.nix
+
+    {
+
       importer = {
+        common.readOnlypkgs.enable = false;
         inputs = {
           homeImport.enable = true;
           impermanance.enable = true;
@@ -10,22 +15,36 @@
           matugen.enable = true;
         };
       };
-      nixpkgs.pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        config = {
-          allowBroken = true;
-          allowUnfree = true;
-        };
-
-        overlays = [ inputs.self.overlays.default ];
+    }
+    ({ inputs, system, config, flakeRoot, ... }: {
+      nixpkgs.hostPlatform = "x86_64-linux";
+      nixpkgs.config = {
+        allowBroken = true;
+        allowUnfree = true;
       };
-      common.specialUser = "steryy";
+
+      # nixpkgs.pkgs = import inputs.nixpkgs {
+      #   system = "x86_64-linux";
+      #   config = {
+      #     allowBroken = true;
+      #     allowUnfree = true;
+      #   };
+      #
+      #   # overlays = [ inputs.self.overlays.default ];
+      # };
+
+      # clan.user-password.user = "alice";
+
+      clan.user-password.user = adminUser;
+
+      # common.specialUser = "steryy";
       services.desktopManager.plasma6.enable = true;
       fileSystems."/persist".neededForBoot = true;
       persistence = {
         rollbacks.btrfs.enable = true;
         device = "/dev/mapper/luksRoot";
       };
+
     })
   ];
 }
