1cc418e0d0ae4812922c10ce23ce6e8c264ea015
steryy <contact@steryy.xyz>
2025-04-18 14:27:51 +0000

    feat: persist someDirs

--- modules/home/homeProfiles/xdgUserDirs.nix
+++ modules/home/homeProfiles/xdgUserDirs.nix
@@ -1,36 +1,56 @@
-{config, ...}: {
-  xdg = {
-    enable = true;
+{ config, lib, options, ... }: {
+  config = lib.mkMerge [
+    (lib.optionalAttrs (options ? "persistence") {
+      persistence.state.directories =
+        map (x: lib.removePrefix "${config.home.homeDirectory}/" x)
+        (lib.attrValues {
+          inherit (config.xdg.userDirs) music videos pictures desktop documents;
+          inherit (config.xdg.userDirs.extraConfig)
+            XDG_SCREENSHOTS_DIR XDG_WALLPAPERS_DIR PROJECTS;
 
-    cacheHome = "${config.home.homeDirectory}/.cache";
-    configHome = "${config.home.homeDirectory}/.config";
-    dataHome = "${config.home.homeDirectory}/.local/share";
-    stateHome = "${config.home.homeDirectory}/.local/state";
+          inherit (config.programs.gpg) homedir;
 
-    userDirs = let
-      h = "${config.home.homeDirectory}";
-      m = "${h}/Media";
-    in {
-      enable = true;
-      createDirectories = true;
+          # inherit (config.xdg.userDirs)
+          #   music videos documents pictures desktop download;
+        });
+    })
+    {
+      xdg = {
+        enable = true;
 
-      music = "${m}/Muzyka";
-      videos = "${m}/Wideo";
-      documents = "${m}/Dokumenty";
-      pictures = "${m}/Obrazy";
-      desktop = "${h}/Pulpit";
-      download = "${h}/Pobrane";
-      publicShare = null;
-      templates = null;
-      extraConfig = {
-        OLLAMA_MODELS = "${config.xdg.stateHome}/ollama";
-        WINEPREFIX = "${config.xdg.dataHome}/wine";
-        PROJECTS = "${config.home.homeDirectory}/projects";
-        RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
-        GPGHOME = config.programs.gpg.homedir;
-        XDG_SCREENSHOTS_DIR = "${config.xdg.userDirs.pictures}/Zrzuty_ekranu";
-        XDG_WALLPAPERS_DIR = "${config.xdg.userDirs.pictures}/Tapety";
+        cacheHome = "${config.home.homeDirectory}/.cache";
+        configHome = "${config.home.homeDirectory}/.config";
+        dataHome = "${config.home.homeDirectory}/.local/share";
+        stateHome = "${config.home.homeDirectory}/.local/state";
+
+        userDirs = let
+          h = "${config.home.homeDirectory}";
+          m = "${h}/Media";
+        in {
+          enable = true;
+          createDirectories = true;
+
+          music = "${m}/Muzyka";
+          videos = "${m}/Wideo";
+          documents = "${m}/Dokumenty";
+          pictures = "${m}/Obrazy";
+          desktop = "${h}/Pulpit";
+          download = "${h}/Pobrane";
+          publicShare = null;
+          templates = null;
+          extraConfig = {
+            OLLAMA_MODELS = "${config.xdg.stateHome}/ollama";
+            WINEPREFIX = "${config.xdg.dataHome}/wine";
+            PROJECTS = "${config.home.homeDirectory}/projects";
+            RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
+            # GPGHOME = config.programs.gpg.homedir;
+            XDG_SCREENSHOTS_DIR =
+              "${config.xdg.userDirs.pictures}/Zrzuty_ekranu";
+            XDG_WALLPAPERS_DIR = "${config.xdg.userDirs.pictures}/Tapety";
+          };
+        };
       };
-    };
-  };
+    }
+
+  ];
 }
