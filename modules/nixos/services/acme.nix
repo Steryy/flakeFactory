b3f2b2590e36f94344cdbadfe60260ff1c01f76f
steryy <contact@steryy.xyz>
2025-04-22 19:45:54 +0200

    feat: update services modules

--- modules/nixos/services/acme.nix
+++ modules/nixos/services/acme.nix
@@ -1,23 +1,76 @@
-{ config, lib, ... }: {
-  systemd.services."acme-fixperms".wants = [ "bind.service" ];
-  systemd.services."acme-fixperms".after = [ "bind.service" ];
-  age.secrets = {
-    "acme.env" = { };
+{ config, lib, inputs, ... }:
+let
+  provider = config.clan.core.vars.generators.acme.files.provider.value;
+  dnsResorvers = { cloudflare = "1.1.1.1:53"; };
+in {
 
-  };
-  security.acme = {
-    acceptTerms = true;
-    certs = lib.pipe (config.networking.domains or [ ]) [
-      (map (u: {
-        name = u;
-        value = {
-          domain = "${u}";
-          extraDomainNames = [ "*.${u}" ];
-          dnsPropagationCheck = true;
-          environmentFile = "${config.age.secrets."acme.env".path}";
+  imports = [ inputs.clan-core.clanModules.nginx ];
+  options.services.nginx.virtualHosts = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule ({ name, ... }:
+      let
+        dom = lib.lists.findFirst (x: lib.strings.hasSuffix x name) null
+          config.networking.domains;
+      in {
+        config = lib.mkIf (dom != null) {
+          acmeRoot = null;
+          useACMEHost = dom;
         };
-      }))
-      lib.listToAttrs
-    ];
+      }));
+  };
+  config = {
+    users.groups.acme.members = [ config.services.nginx.user ];
+    # users.users."${config.services.nginx.user}".extra
+    systemd.services."acme-fixperms".wants = [ "bind.service" ];
+    systemd.services."acme-fixperms".after = [ "bind.service" ];
+
+    clan.core = {
+      vars.generators.acme = {
+
+        files = { provider = { secret = false; }; };
+        prompts = {
+
+          "prov" = {
+            type = "line";
+            description = "Set provider (${
+                lib.strings.concatStringsSep " " (lib.attrNames dnsResorvers)
+              })";
+          };
+          "acme.env" = {
+            type = "multiline";
+            description = "Put provider env vars";
+            persist = true;
+          };
+        };
+        script = ''
+          cp $prompts/prov $out/provider
+        '';
+      };
+    };
+
+    security.acme = {
+      acceptTerms = true;
+      defaults = {
+        dnsProvider = provider;
+
+        dnsResolver = if dnsResorvers ? "${provider}" then
+          dnsResorvers."${provider}"
+        else
+          throw "${provider} not supported";
+
+        environmentFile =
+          config.clan.core.vars.generators.acme.files."acme.env".path;
+      };
+      certs = lib.pipe (config.networking.domains) [
+        (map (u: {
+          name = u;
+          value = {
+            domain = "${u}";
+            extraDomainNames = [ "*.${u}" ];
+            dnsPropagationCheck = true;
+          };
+        }))
+        lib.listToAttrs
+      ];
+    };
   };
 }
--- modules/nixos/services/hosted/nextcloud.nix
+++ modules/nixos/services/hosted/nextcloud.nix
@@ -1,24 +1,83 @@
-{ config, ... }:
-let hostname = "nextcloud.${config.networking.domain}";
+{ config, lib, pkgs, ... }:
+let
+  domain = lib.head config.networking.domains;
+  hostname = "nextcloud.${domain}";
 in {
   config = {
-    age.secrets = { "nextcloudAdmin" = { }; };
-    networking.exposedServices.nextcloud = { port = 11000; };
+
+    clan.core = {
+      state.nextcloud.folders = [
+        config.services.nextcloud.home
+        # "/var/lib/tailscale"
+      ];
+      vars.generators.nextcloud = {
+        prompts.admin-password.type = "hidden";
+        prompts.admin-password.persist = true;
+        prompts.admin-password.description =
+          "You can autogenerate a password, if you leave this prompt blank.";
+        files.admin-password.deploy = false;
+        files.admin-password-hash = { };
+
+        runtimeInputs = [ pkgs.coreutils pkgs.xkcdpass pkgs.mkpasswd ];
+        script = ''
+          prompt_value=$(cat "$prompts"/admin-password)
+          if [[ -n "''${prompt_value-}" ]]; then
+            echo "$prompt_value" | tr -d "\n" > "$out"/admin-password
+          else
+            xkcdpass --numwords 3 --delimiter - --count 1 | tr -d "\n" > "$out"/admin-password
+          fi
+          mkpasswd -s -m sha-512 < "$out"/admin-password | tr -d "\n" > "$out"/admin-password-hash
+        '';
+
+      };
+    };
+
+    # networking.exposedServices.nextcloud = { port = 11000; };
+
+    clan.postgresql.users.nextcloud = { };
+    clan.postgresql.databases.nextcloud.create.options = {
+      TEMPLATE = "template0";
+      LC_COLLATE = "C";
+      LC_CTYPE = "C";
+      ENCODING = "UTF8";
+      OWNER = "nextcloud";
+    };
+    clan.postgresql.databases.nextcloud.restore.stopOnRestore = [ "nextcloud" ];
+
     services.nextcloud = {
       enable = true;
+      package = with pkgs; nextcloud31;
       hostName = hostname;
-      config.adminpassFile = config.age.secrets."nextcloudAdmin".path;
-      settings.trusted_domains =
-        map (x: "nextcloud.${x}") config.networking.domains;
+      home = "/var/lib/nextcloud";
+      datadir = "/var/lib/nextcloud";
+      https = true;
+      database.createLocally = true;
+      configureRedis = true;
+      config = {
+        dbtype = "pgsql";
+        dbname = "nextcloud";
+        dbuser = "nextcloud";
+        adminuser = "nextadmin";
+
+        adminpassFile =
+          config.clan.core.vars.generators.nextcloud.files.admin-password-hash.path;
+      };
+      maxUploadSize = "2G"; # also sets post_max_size and memory_limit
+      phpOptions = { "opcache.interned_strings_buffer" = "16"; };
       settings = {
-        overwriteprotocol = "https";
+        # overwriteprotocol = "https";
         trusted_proxies = [ "127.0.0.1" ];
       };
       extraAppsEnable = true;
     };
-    services.nginx.virtualHosts."${hostname}".listen = [{
-      addr = "127.0.0.1";
-      port = config.exposedServices.nextcloud.port;
-    }];
+    services.nginx = {
+      enable = true;
+      virtualHosts = {
+        "${hostname}" = {
+          forceSSL = true;
+          # enableACME = true;
+        };
+      };
+    };
   };
 }
