0cff890b1e61d6fe2bd5d775c4f5a97d33c6ee5c
steryy <contact@steryy.xyz>
2025-04-29 11:45:45 +0000

    refactor: change extensions to home manager native

--- modules/home/homeProfiles/programs/thunderbird.nix
+++ modules/home/homeProfiles/programs/thunderbird.nix
@@ -1,78 +1,117 @@
-{
-  lib,
-  options,
-  config,
-  pkgs,
-  ...
-}: let
-  # file = ./mocha-mauve.xpi;
-  thund = "https://addons.thunderbird.net/thunderbird/downloads/";
-  arr = {
-    "*".installation_mode = "blocked";
-    # "{47f5c9df-1d03-5424-ae9e-0613b69a9d2f}" = {
-    #   install_url = "file://${file}";
-    #   installation_mode = "force_installed";
-    # };
-    "gmail-labels@itln.pl" = {
-      installation_mode = "force_installed";
-      install_url = "${thund}file/1031572/gmail_labels-0.3-tb.xpi";
-    };
-    "dkim_verifier@pl" = {
-      installation_mode = "force_installed";
-      install_url = "${thund}latest/dkim-verifier/addon-438634-latest.xpi";
-    };
-    "x0h44fx9x@relay.firefox.com" = {
-      installation_mode = "force_installed";
-      install_url = "${thund}latest/openpgp-alias-updater/addon-988108-latest.xpi";
-    };
-    "gconversation@xulforum.org" = {
-      installation_mode = "force_installed";
-      install_url = "${thund}file/1028919/thunderbird_conversations-4.1.7-tb.xpi";
-    };
-    # "admin@fastaddons.com_Darko" = {
-    #   installation_mode = "force_installed";
-    #   install_url = "${thund}latest/darko_t/addon-987944-latest.xpi";
-    # };
-  };
+{ lib, options, inputs, pkgs, ... }:
+let
+  inherit (inputs.nur.legacyPackages."${pkgs.system}".repos.rycee.firefox-addons)
+    buildFirefoxXpiAddon british-english-dictionary-2 polish-dictionary;
+  extensions = [
+    british-english-dictionary-2
+    polish-dictionary
+
+    (buildFirefoxXpiAddon rec {
+      pname = "thunderAI";
+      version = "v2.2.0pre5";
+      addonId = "thunderai@micz.it";
+      url =
+        "https://github.com/micz/ThunderAI/releases/download/v2.2.0pre5/thunderai-${version}.xpi";
+      sha256 = "NSEMCAlVWH22sY0Z5zEYltrbU8CS1RdG5CXIxerpJh0=";
+      meta = { };
+    })
+    (buildFirefoxXpiAddon rec {
+      pname = "cardbook";
+      version = "97.5";
+      addonId = "cardbook@vigneau.philippe";
+      url =
+        "https://addons.thunderbird.net/user-media/addons/_attachments/634298/cardbook-${version}-tb.xpi";
+      sha256 =
+        "d2bef178398979d7e0e055b1259c59995302e4151bf471146073c1bbd88cc657";
+      meta = with lib; {
+        homepage = "https://gitlab.com/CardBook/CardBook";
+        description =
+          "This add-on allows you to manage all your contacts under the vCard standard.";
+        license = licenses.mpl20;
+        mozPermissions = [ ];
+        platforms = platforms.all;
+      };
+    })
+    (buildFirefoxXpiAddon rec {
+      addonId = "quote_colors_collapse@thunderbird-mail.de";
+      pname = "quote-colors-collapse";
+      version = "4.2.4";
+      url =
+        "https://addons.thunderbird.net/user-media/addons/_attachments/987892/quote_colors_collapse-${version}-tb.xpi";
+      sha256 =
+        "0d3c31e5b57add80b00d2b93297154ea6b208e50bea7489d020946438d0e56db";
+      meta = {
+
+        homepage =
+          "https://addons.thunderbird.net/thunderbird/addon/quotecolors/";
+        mozPermissions = [
+
+          "messagesRead"
+          "messagesModify"
+          "compose"
+          "storage"
+          "tabs"
+        ];
+
+      };
+    })
+    (buildFirefoxXpiAddon {
+      pname = "languagetool";
+      version = "8.11.2";
+      addonId = "languagetool-mailextension@languagetool.org";
+      url =
+        "https://addons.thunderbird.net/thunderbird/downloads/file/1031394/grammatik_und_rechtschreibprufung_languagetool-8.11.2-tb.xpi";
+      sha256 = "gBaKJIXjPtchXTNUKTRjD9iZ2OSqZGlw7xMjjsaK8rw=";
+      meta = { };
+    })
+  ];
 in {
   config = lib.mkMerge [
-    (
-      lib.optionalAttrs (options ? "persistence")
-      {
-        persistence.state.directories = [
-          ".thunderbird"
-        ];
-      }
-    )
+    (lib.optionalAttrs (options ? "persistence") {
+      persistence.state.directories = [ ".thunderbird" ];
+    })
     {
-      accounts.email.accounts.gmailmain.thunderbird = {
-        enable = true;
-        profiles = ["first"];
-        settings = id: {
-          "mail.smtpserver.smtp_${id}.authMethod" = 10;
-          "mail.server.server_${id}.authMethod" = 10;
-          # "mail.openpgp.alias_rules_file" = "openpgp_alias.json";
-          # "mail.openpgp.alias_rules_file" = "file://${alias-rules-file}";
-          "mail.openpgp.fetch_pubkeys_from_gnupg" = true;
-        };
-      };
       programs = {
         thunderbird = {
+          settings = {
+            "widget.wayland.use-move-to-rect" = false; # NOTE: workaround
+            "mail.biff.show_tray_icon_always" = true;
+            "mail.minimizeToTray" = true;
+            "ldap_2.servers.outlook.dirType" = 3;
+            "dom.security.unexpected_system_load_telemetry_enabled" = false;
+            "network.trr.confirmation_telemetry_enabled" = false;
+            "privacy.trackingprotection.origin_telemetry.enabled" = false;
+            "telemetry.origin_telemetry_test_mode.enabled" = false;
+            "toolkit.telemetry.archive.enabled" = false;
+            "toolkit.telemetry.bhrPing.enabled" = false;
+            "toolkit.telemetry.ecosystemtelemetry.enabled" = false;
+            "toolkit.telemetry.firstShutdownPing.enabled" = false;
+            "toolkit.telemetry.newProfilePing.enabled" = false;
+            "toolkit.telemetry.shutdownPingSender.enabled" = false;
+            "toolkit.telemetry.shutdownPingSender.enabledFirstSession" = false;
+            "toolkit.telemetry.updatePing.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.rejected" = true;
+            "toolkit.telemetry.prompted" = 2;
+          };
           enable = true;
           # package = pkgs.betterbird;
-          package = pkgs.wrapThunderbird pkgs.thunderbird-unwrapped {
-            extraPolicies = {
-              DisableTelemetry = true;
-              # add policies here...
-
-              # ---- EXTENSIONS ----
-              ExtensionSettings = arr;
-            };
-          };
+          # package = pkgs.wrapThunderbird pkgs.thunderbird-unwrapped {
+          #   extraPolicies = {
+          #     DisableTelemetry = true;
+          #     # add policies here...
+          #
+          #     # ---- EXTENSIONS ----
+          #     ExtensionSettings = arr;
+          #   };
+          # };
           # inherit settings;
 
           profiles = {
             first = {
+              inherit extensions;
+
               isDefault = true;
               withExternalGnupg = true;
               # extraConfig = ''
