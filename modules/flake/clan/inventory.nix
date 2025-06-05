9257d42c8189d23b4c5b97f61d59e944b14b26bc
steryy <contact@steryy.xyz>
2025-06-05 14:43:33 +0200

    fix: use clan tags to import modules

--- modules/flake/clan/inventory.nix
+++ modules/flake/clan/inventory.nix
@@ -1,6 +1,6 @@
-{inputs,config, ...}:
+{inputs,config, lib, ...}:
 let
-  nixModules = config.haumea.nixModules;
+  nixModules = config.haumea.clan.tags or {};
 in 
 {
   clan = {
@@ -16,25 +16,18 @@ in
 
         };
 
-        importer = {
-          all.roles.default = {
-            tags = ["nixos"];
-            extraModules =
-              (builtins.attrValues nixModules.all)
-              ++ [
-                inputs.clan-core.clanModules.static-hosts
-                {
-                  clan = {
-                    mycelium-static-hosts = {
-                      topLevelDomain = "mc";
-                    };
-                  };
-                clan.core.networking.buildHost = "root@localhost";
-              }
-            ];
-          };
-          headless.roles.default = {
-            tags = ["villainess" "shou" "seirei"];
+        importer = 
+          (lib.mapAttrs (n: v: {
+            roles.default = {
+              tags = [n];
+              extraModules = 
+                lib.collect (x: lib.isPath x) 
+                (lib.filterAttrs (n: _: (n == "required") || n == "default"   ) v )
+                ;
+            };
+
+          }) nixModules) //
+          {
             extraModules = with inputs.srvos.nixosModules; [
               server
               mixins-telegraf
