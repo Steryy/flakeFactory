dfca053b0b1896d26660ddd94de48309b88a5dc1
steryy <contact@steryy.xyz>
2025-06-05 14:37:38 +0200

    feat: use tags modules

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@ treefmt-nix.follows = "treefmt-nix";
         diskoModules = {
           src = ./modules/disko;
         };
-        clanServices = { src = ./modules/clan; };
+        clan = { src = ./modules/clan; };
       };
       imports =
         flakeModules;
--- modules/flake/clan/machines.nix
+++ modules/flake/clan/machines.nix
@@ -10,8 +10,12 @@ let
 
     lib.listToAttrs
   ];
-  nixModules = config.haumea.nixModules;
-  homeModules = config.haumea.homeModules;
+  nixModules =
+    config.haumea.nixModules
+    // {
+      tags = config.haumea.clan.tags or {};
+    };
+  homeModules = config.haumea.homeModules or {};
 
   specialArgs = { inherit flakeRoot inputs homeModules; 
     extraInputs = config.partitions.extraInputs.extraInputs; };
@@ -49,23 +53,47 @@ in {
           name = lib.head (lib.splitString "-" n);
           value = {
             machines."${n}" = let
+              tags = config.clan.inventory.machines."${n}".tags;
               impr = enable:
                 lib.pipe v.importer [
+                  (lib.mapAttrs (nam: lib.filterAttrs (tag: _:
+                      let 
+                        y = if nam == "tags" then
+                          lib.elem tag tags
+                        else true;
+
+                      in
+                    lib.warnIfNot y "To import machine ${n} must be tagged with ${tag}" y
+                    )))
                   (lib.mapAttrsRecursiveCond (x: ! x ? "enable") (p: v: {
                     path = p;
                     enable = v.enable;
                   }))
+
                   (lib.collect (x: x  ? "path" && x ? "enable" && x.enable == enable))
+
+                  (lib.filter (x: 
+                    let
+                      y = if (lib.elemAt x.path 0 == "tags") then 
+                            (lib.elemAt x.path 2 != "required")
+                          else true;
+                    in 
+                      lib.throwIfNot y "Required cannot be imported ${n}" y
+                  ))
+
+                  (lib.filter (x:  lib.hasAttrByPath x.path nixModules ))
                   (map (x: lib.getAttrFromPath x.path nixModules))
                 ];
+              disabledModules = impr false;
             in {
               extraModules =
                 (impr true)
-                ++ [
+                ++ 
+                lib.optional (lib.length  disabledModules  >0 )
                   {
-                    disabledModules = impr false;
+                  inherit disabledModules;
                   }
-                ];
+                ;
             };
           };
         }) le;
