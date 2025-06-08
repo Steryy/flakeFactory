0bc363d1831719296fa6e8f166f58747c1a7e53a
steryy <contact@steryy.xyz>
2025-06-08 16:33:34 +0200

    feat: add region tag

---
+++ modules/flake/terranix/cloud-regions.nix
@@ -0,0 +1,163 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cloud = {
+    shou = {
+      regions = [
+        "af-south-1"
+        "ap-east-1"
+        "ap-northeast-1"
+        "ap-northeast-2"
+        "ap-northeast-3"
+        "ap-south-1"
+        "ap-south-2"
+        "ap-southeast-1"
+        "ap-southeast-2"
+        "ap-southeast-3"
+        "ap-southeast-4"
+        "ap-southeast-5"
+        "ap-southeast-7"
+        "ca-central-1"
+        "ca-west-1"
+        "cn-north-1"
+        "cn-northwest-1"
+        "eu-central-1"
+        "eu-central-2"
+        "eu-north-1"
+        "eu-south-1"
+        "eu-south-2"
+        "eu-west-1"
+        "eu-west-2"
+        "eu-west-3"
+        "il-central-1"
+        "me-central-1"
+        "me-south-1"
+        "mx-central-1"
+        "sa-east-1"
+        "us-east-1"
+        "us-east-2"
+        "us-west-1"
+        "us-west-2"
+      ];
+    };
+
+    seirei = {
+      regions = [
+        "southafricanorth"
+        "southafricawest"
+        "australiacentral"
+        "australiacentral2"
+        "australiaeast"
+        "australiasoutheast"
+        "centralindia"
+        "eastasia"
+        "japaneast"
+        "japanwest"
+        "jioindiacentral"
+        "jioindiawest"
+        "koreacentral"
+        "koreasouth"
+        "newzealandnorth"
+        "southindia"
+        "southeastasia"
+        "westindia"
+        "canadacentral"
+        "canadaeast"
+        "francecentral"
+        "francesouth"
+        "germanynorth"
+        "germanywestcentral"
+        "italynorth"
+        "northeurope"
+        "norwayeast"
+        "norwaywest"
+        "polandcentral"
+        "spaincentral"
+        "swedencentral"
+        "switzerlandnorth"
+        "switzerlandwest"
+        "uksouth"
+        "ukwest"
+        "westeurope"
+        "mexicocentral"
+        "israelcentral"
+        "qatarcentral"
+        "uaecentral"
+        "uaenorth"
+        "brazilsouth"
+        "brazilsoutheast"
+        "brazilus"
+        "centralus"
+        "centraluseuap"
+        "eastus"
+        "eastus2"
+        "eastus2euap"
+        "eastusstg"
+        "northcentralus"
+        "southcentralus"
+        "southcentralusstg"
+        "westcentralus"
+        "westus"
+        "westus2"
+        "westus3"
+      ];
+    };
+  };
+
+  types = lib.attrNames cloud;
+  getSpecialTag = tag: tags: let
+    sp = lib.filter (lib.hasPrefix "${tag}:") tags;
+  in
+    if lib.length sp == 0
+    then null
+    else
+      lib.removePrefix "${tag}:"
+      (lib.head sp);
+
+  cloudMachines = lib.pipe config.clan.hosts [
+    (lib.filterAttrs (_: v: builtins.any (x: lib.elem x v) types))
+    (lib.mapAttrs (_: v: {
+      type = lib.findFirst (x: lib.elem x v) null types;
+      region = getSpecialTag "region" v;
+    }))
+  ];
+in {
+  options.clan.cloudLocations = lib.mkOption {
+    type = lib.types.attrsOf (
+      lib.types.submodule {
+        options = {
+          regions = lib.mkOption {
+            type = lib.types.listOf (
+              lib.types.str
+            );
+          };
+        };
+      }
+    );
+    default = cloud;
+  };
+
+  config = {
+    flake.cloudMachines = cloudMachines;
+    clan.machines =
+      lib.mapAttrs (n: v: {
+        assertions = [
+          {
+            assertion = v.region != null;
+            message = "Machine ${n} has no region defined";
+          }
+          {
+            assertion =
+              (lib.elem v.region cloud."${v.type}".regions);
+            message = "Region ${toString v.region} is not valid for machine ${n}";
+          }
+        ];
+      })
+      cloudMachines;
+    clan.specialArgs = {
+      cloudLocations = cloud;
+    };
+  };
+}
