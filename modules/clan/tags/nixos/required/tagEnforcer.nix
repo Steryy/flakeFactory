2c76e5376dd6c46a0af7c232e2567e7c894bbfb8
steryy <contact@steryy.xyz>
2025-06-10 12:51:34 +0200

    feat: change tag and documentation

--- modules/clan/tags/nixos/required/tagEnforcer.nix
+++ modules/clan/tags/nixos/required/tagEnforcer.nix
@@ -1,46 +1,61 @@
-{ config, lib, ... }:
-let
+{
+  config,
+  lib,
+  ...
+}: let
   tag =
-    lib.filter (x: !lib.elem x [ "all" "nixos" ]) config.clan.inventory.tags;
-  serverProviders = [ "villainess" "shou" "seirei" ];
+    lib.filter (x: !lib.elem x ["all" "nixos"]) config.clan.inventory.tags;
+  serverProviders = ["villainess" "shou" "seirei"];
   isKami = lib.elem "kami" tag;
-  factions = (lib.filter (x: lib.hasPrefix "faction-" x) tag);
-  gatherFactions = lib.pipe config.clan.inventory.machines [
-    (lib.filterAttrs (_: v:
-      let
-        len = lib.length factions;
-        faction = lib.head factions;
-      in if len == 1 then lib.elem faction v.tags else false))
-    (lib.filterAttrs (_: v: lib.elem "shikikan" v.tags))
-    lib.attrValues
-    lib.length
-  ];
-in {
-  assertions = (lib.optionals (isKami) [{
-    assertion = !(lib.any (x: lib.elem x tag)
-      (serverProviders ++ [ "senkan" "shikikan" ]));
-    message = "Kami tags are only allowed on gui hosts";
-  }]) ++ (lib.optionals (!isKami) [
-    {
 
-      assertion = lib.length (lib.filter (x: lib.elem x tag) serverProviders)
-        == 1;
-      message = "Only one server provider is allowed per host";
-    }
-    {
-      assertion = (lib.any (x: lib.elem x tag) [ "senkan" "shikikan" ])
-        == (lib.length factions == 1);
-      message = "Only one faction is allowed for Kubernetes";
-    }
-    {
-      assertion = (lib.any (x: lib.elem x tag) [ "senkan" "shikikan" ])
-        == (gatherFactions == 1);
-      message = "Only one controller is allowed in faction";
-    }
-    {
+  rolesCube = lib.filter (x: lib.any (y: lib.hasPrefix "${y}:" x) ["senkan" "shikikan"]) tag;
+  split = lib.splitString ":";
+  faction = lib.elemAt (split (lib.head rolesCube)) 1;
+
+  getRoles = lib.pipe config.clan.inventory.machines [
+    (lib.mapAttrs (_: v: lib.filter (x: lib.hasSuffix ":${faction}" x) v.tags))
+    (lib.filterAttrs (_: v: lib.length v == 0))
+    (lib.mapAttrs (_: v: lib.head (split (lib.head v))))
+    lib.attrValues
+    (lib.groupBy (x: x))
+    (lib.mapAttrs (_: lib.length))
+  ];
+
+
+  providers = lib.filter (x: lib.elem x tag) serverProviders;
+in {
+  assertions =
+    (lib.optional isKami {
       assertion =
-        lib.length (lib.filter (x: lib.elem x tag) [ "senkan" "shikikan" ]) < 2;
-      message = "Kubernetes controller cant be Kubernetes node";
-    }
-  ]);
+        !(lib.any (x: lib.elem x tag)
+          serverProviders);
+      message = "Kami tags are only allowed on gui hosts";
+    })
+    ++ (
+      lib.optional (!isKami)
+      {
+        assertion =
+          lib.length providers
+          == 1;
+        message = "Only one server provider is allowed per host";
+      }
+    )
+    ++ (lib.optionals (lib.length rolesCube > 0) [
+      {
+        assertion = !isKami;
+        message = "Kami tags are only allowed on gui hosts";
+      }
+      {
+        assertion = lib.length rolesCube == 1;
+        message = "Only one role is allowed for host";
+      }
+      {
+        assertion = getRoles ? "shikikan"  && getRoles ? "senkan";
+        message = "Faction ${faction} needs to have at least one `shikikan` and one `senkan`";
+      }
+      {
+        assertion =  getRoles ? "shikikan" && getRoles.senkan == 1;
+        message = "Only one controller is allowed in faction ${faction}";
+      }
+    ]);
 }
