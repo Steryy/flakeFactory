ca8a2e906a3e41d8fa0e28f470554bb565c7491b
steryy <contact@steryy.xyz>
2025-06-30 11:57:49 +0200

    refactor: remove unneeded code

--- modules/clan/tags/nixos/required/tagEnforcer.nix
+++ modules/clan/tags/nixos/required/tagEnforcer.nix
@@ -7,21 +7,6 @@
     lib.filter (x: !lib.elem x ["all" "nixos"]) config.clan.inventory.tags;
   serverProviders = ["villainess" "shou" "seirei"];
   isKami = lib.elem "kami" tag;
-
-  rolesCube = lib.filter (x: lib.any (y: lib.hasPrefix "${y}:" x) ["senkan" "shikikan"]) tag;
-  split = lib.splitString ":";
-  faction = lib.elemAt (split (lib.head rolesCube)) 1;
-
-  getRoles = lib.pipe config.clan.inventory.machines [
-    (lib.mapAttrs (_: v: lib.filter (x: lib.hasSuffix ":${faction}" x) v.tags))
-    (lib.filterAttrs (_: v: lib.length v == 0))
-    (lib.mapAttrs (_: v: lib.head (split (lib.head v))))
-    lib.attrValues
-    (lib.groupBy (x: x))
-    (lib.mapAttrs (_: lib.length))
-  ];
-
-
   providers = lib.filter (x: lib.elem x tag) serverProviders;
 in {
   assertions =
@@ -39,23 +24,5 @@ in {
           == 1;
         message = "Only one server provider is allowed per host";
       }
-    )
-    ++ (lib.optionals (lib.length rolesCube > 0) [
-      {
-        assertion = !isKami;
-        message = "Kami tags are only allowed on gui hosts";
-      }
-      {
-        assertion = lib.length rolesCube == 1;
-        message = "Only one role is allowed for host";
-      }
-      {
-        assertion = getRoles ? "shikikan"  && getRoles ? "senkan";
-        message = "Faction ${faction} needs to have at least one `shikikan` and one `senkan`";
-      }
-      {
-        assertion =  getRoles ? "shikikan" && getRoles.senkan == 1;
-        message = "Only one controller is allowed in faction ${faction}";
-      }
-    ]);
+    );
 }
--- modules/flake/terranix/cloud-regions.nix
+++ modules/flake/terranix/cloud-regions.nix
@@ -3,145 +3,25 @@
   config,
   ...
 }: let
-  inherit (lib.local.tags) getSpecial;
-  cloud = {
-    shou = {
-      default = "eu-central-1";
-      regions = [
-        "af-south-1"
-        "ap-east-1"
-        "ap-northeast-1"
-        "ap-northeast-2"
-        "ap-northeast-3"
-        "ap-south-1"
-        "ap-south-2"
-        "ap-southeast-1"
-        "ap-southeast-2"
-        "ap-southeast-3"
-        "ap-southeast-4"
-        "ap-southeast-5"
-        "ap-southeast-7"
-        "ca-central-1"
-        "ca-west-1"
-        "cn-north-1"
-        "cn-northwest-1"
-        "eu-central-1"
-        "eu-central-2"
-        "eu-north-1"
-        "eu-south-1"
-        "eu-south-2"
-        "eu-west-1"
-        "eu-west-2"
-        "eu-west-3"
-        "il-central-1"
-        "me-central-1"
-        "me-south-1"
-        "mx-central-1"
-        "sa-east-1"
-        "us-east-1"
-        "us-east-2"
-        "us-west-1"
-        "us-west-2"
-      ];
-    };
-
-    seirei = {
-      default = "germanywestcentral";
-      regions = [
-        "southafricanorth"
-        "southafricawest"
-        "australiacentral"
-        "australiacentral2"
-        "australiaeast"
-        "australiasoutheast"
-        "centralindia"
-        "eastasia"
-        "japaneast"
-        "japanwest"
-        "jioindiacentral"
-        "jioindiawest"
-        "koreacentral"
-        "koreasouth"
-        "newzealandnorth"
-        "southindia"
-        "southeastasia"
-        "westindia"
-        "canadacentral"
-        "canadaeast"
-        "francecentral"
-        "francesouth"
-        "germanynorth"
-        "germanywestcentral"
-        "italynorth"
-        "northeurope"
-        "norwayeast"
-        "norwaywest"
-        "polandcentral"
-        "spaincentral"
-        "swedencentral"
-        "switzerlandnorth"
-        "switzerlandwest"
-        "uksouth"
-        "ukwest"
-        "westeurope"
-        "mexicocentral"
-        "israelcentral"
-        "qatarcentral"
-        "uaecentral"
-        "uaenorth"
-        "brazilsouth"
-        "brazilsoutheast"
-        "brazilus"
-        "centralus"
-        "centraluseuap"
-        "eastus"
-        "eastus2"
-        "eastus2euap"
-        "eastusstg"
-        "northcentralus"
-        "southcentralus"
-        "southcentralusstg"
-        "westcentralus"
-        "westus"
-        "westus2"
-        "westus3"
-      ];
-    };
+  inherit (lib.local.tags) getSpecial groups;
+  inherit (lib.local.terranix) regionsForCloud;
+  defaultRegions = {
+    shou = "eu-central-1";
+    seirei = "germanywestcentral";
   };
 
-  types = lib.attrNames cloud;
   cloudMachines = lib.pipe config.clan.hosts [
-    (lib.filterAttrs (_: v: builtins.any (x: lib.elem x v) types))
+    (lib.filterAttrs (_: v: builtins.any (x: lib.elem x v) groups.cloudProviders))
     (lib.mapAttrs (_: v: {
-      type = lib.findFirst (x: lib.elem x v) null types;
+      type = lib.findFirst (x: lib.elem x v) null groups.cloudProviders;
       region = getSpecial "region" v;
     }))
   ];
 in {
-  options.clan.cloudLocations = lib.mkOption {
-    type = lib.types.attrsOf (
-      lib.types.submodule {
-        options = {
-          default = lib.mkOption {
-            type = lib.types.str;
-          };
-          regions = lib.mkOption {
-            type = lib.types.listOf (
-              lib.types.str
-            );
-          };
-        };
-      }
-    );
-    default = cloud;
-  };
-
   config = {
-    flake.cloudMachines = cloudMachines;
-
     clan.inventory.machines =
       lib.mapAttrs (n: v: let
-        def = cloud."${v.type}".default;
+        def = defaultRegions."${v.type}";
       in {
         tags =
           if v.region == null
@@ -153,20 +33,17 @@ in {
       lib.mapAttrs (n: v: let
         reg =
           if v.region == null
-          then cloud."${v.type}".default
+          then defaultRegions."${v.type}"
           else v.region;
       in {
         assertions = [
           {
             assertion =
-              lib.elem reg cloud."${v.type}".regions;
+              lib.elem reg regionsForCloud."${v.type}";
             message = "Region ${toString v.region} is not valid for machine ${n}";
           }
         ];
       })
       cloudMachines;
-    clan.specialArgs = {
-      cloudLocations = cloud;
-    };
   };
 }
