ec35ac1c85bdcf1562395ab51650807f5e1b9ca2
steryy <contact@steryy.xyz>
2025-07-12 17:12:23 +0200

    feat: refactor clan services and improve tailscale

---
+++ modules/clan/services/tailscale/client.nix
@@ -0,0 +1,51 @@
+{
+  settings,
+  machine,
+  roles,
+  ...
+}: {
+  nixosModule = {lib, ...}: let
+    controller = lib.head (lib.mapAttrsToList (n: v: {
+      name = n;
+      inherit (v) settings;
+    }) (roles.headscale.machines or {}));
+    rol = machine.roles;
+    authKey = "/var/lib/tailscale/preAuth/pre-auth-key";
+  in {
+    config = lib.mkIf (! lib.elem "headscale" rol) {
+      systemd.services."sshd-get-tailscale" = {
+        wantedBy = ["tailscaled-autoconnect.service"];
+        unitConfig = {
+          ConditionPathExists = "!${authKey}";
+        };
+        before = ["tailscaled-autoconnect.service"];
+      };
+      services.tailscale = {
+        enable = true;
+        openFirewall = true;
+        inherit (settings) useRoutingFeatures;
+        authKeyFile =
+          authKey;
+        authKeyParameters = {
+          preauthorized =
+            null;
+          ephemeral = null;
+        };
+
+        extraUpFlags =
+          [
+            "--accept-routes"
+            "--login-server=${controller.settings.publicUrl}"
+          ]
+          ++ (lib.optional (settings.advertised-rotes != [])
+            "--advertise-routes=${
+              lib.concatStringsSep "," settings.advertised-rotes
+            }");
+        # map (x: "--advertise-routes=${exposedIps}") settings.advertised-rotes;
+      };
+      clan.core = {
+        state.tailscale.folders = ["/var/lib/tailscale"];
+      };
+    };
+  };
+}
---
+++ modules/clan/services/tailscale/server.nix
@@ -0,0 +1,63 @@
+{
+  settings,
+  roles,
+  ...
+}: {
+  nixosModule = {
+    pkgs,
+    hostName,
+    lib,
+    ...
+  }: let
+    clients = lib.removeAttrs roles.client.machines [hostName];
+  in {
+    clan.core = {
+      state.headscale.folders = ["/var/lib/headscale"];
+    };
+
+    clan.nginx.acme.email = "contact@stanley-dev.net";
+
+    services = {
+      headscale = {
+        enable = true;
+        port = 8087;
+        settings = {
+          policy.path = let
+            jsonFormat = pkgs.formats.json {};
+          in
+            jsonFormat.generate "policy.json" {
+              autoApprovers = {
+                routes = lib.listToAttrs (
+                  lib.flatten (
+                    lib.mapAttrsToList (n: v:
+                      map (x: {
+                        name = x;
+                        value = ["tag:host-${n}"];
+                      })
+                      v.settings.advertised-rotes)
+                    clients
+                  )
+                );
+              };
+            };
+          logtail.enabled = false;
+          server_url = "${settings.publicUrl}";
+          dns = {
+            magic_dns = true;
+            base_domain = settings.tld;
+            search_domains = [
+              settings.domain
+            ];
+            nameservers.global = [
+              "1.1.1.1"
+            ];
+          };
+          # ip_prefixes = [
+          #   "fd7a:115c:a1e0::/48"
+          #   "100.64.0.0/10"
+          # ];
+        };
+      };
+    };
+  };
+}
