139c1fa8ab9ae5881c8817b50f6dad9b36493475
steryy <contact@steryy.xyz>
2025-07-12 17:24:03 +0200

    refactor: user groups of tags

--- modules/flake/clan/inventory/default.nix
+++ modules/flake/clan/inventory/default.nix
@@ -1,95 +1,23 @@
-
-{
-  inputs,
-  config,
-  lib,
-  ...
-}: let
-  nixModules = config.haumea.clan.tags or {};
-  inherit (lib.local.tags) toInventory getAll groups;
-  allTags = getAll config.clan.inventory.machines;
-  toInv = toInventory allTags;
+{lib, ...}: let
   inherit (lib.local.keys) fileFromGroup;
 in {
   clan = {
     inventory = {
-      instances =
-        {
-          zt = {
-            module = {
-              name = "zerotier";
-              input = "clan-core";
-            };
-            roles.peer.tags.all = {};
-            roles.moon.machines ={};
-            roles.controller.machines.shou-jeannette = {
-              settings = {
-                allowedIps = [];
-              };
-            };
+      instances = {
+        zt = {
+          module = {
+            name = "zerotier";
+            input = "clan-core";
           };
-        }
-        // (lib.mapAttrs' (n: v: {
-            name = "import-${n}";
-            value = {
-              module = {
-                name = "importer";
-                input = "clan-core";
-              };
-              roles.default = {
-                extraModules =
-                  lib.collect (x: lib.isPath x)
-                  (lib.filterAttrs (n: _: (n == "required") || n == "default") v);
-                tags =
-                  toInv [n];
-              };
-            };
-          })
-          nixModules)
-        // {
-          import-server = {
-            module = {
-              name = "importer";
-              input = "clan-core";
-            };
-            roles.default = {
-              tags = toInv (groups.server);
-              extraModules = with inputs.srvos.nixosModules; [
-                server
-                mixins-telegraf
-              ];
-            };
-          };
-          import-desktop = {
-            module = {
-              name = "importer";
-              input = "clan-core";
-            };
-            roles.default = {
-              tags = toInv (groups.desktop);
-              extraModules = with inputs.srvos.nixosModules; [
-                desktop
-                mixins-systemd-boot
-                mixins-nix-experimental
-              ];
-            };
-          };
-          import-all = {
-            module = {
-              name = "importer";
-              input = "clan-core";
-            };
-            roles.default = {
-              tags.all = {};
-              extraModules = [
-                ../../options.nix
-                {
-                  clan.inventory.machines = config.clan.inventory.machines;
-                }
-              ];
+          roles.peer.tags.all = {};
+          roles.moon.machines = {};
+          roles.controller.machines.shou-jeannette = {
+            settings = {
+              allowedIps = [];
             };
           };
         };
+      };
       services = {
         user-password.default = {roles.default.tags = ["kami"];};
         state-version.default = {roles.default.tags = ["all"];};
