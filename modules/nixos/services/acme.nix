5ed253c1543ac26cdf25d22cb07954048aaf951e
steryy <contact@steryy.xyz>
2025-07-12 18:23:48 +0200

    feat: use ssh-share service

--- modules/nixos/services/acme.nix
+++ modules/nixos/services/acme.nix
@@ -3,36 +3,45 @@ let
   provider = config.clan.core.vars.generators.acme.files.provider.value;
   dnsResorvers = { cloudflare = "1.1.1.1:53"; };
 in {
-
-  options.services.nginx.virtualHosts = lib.mkOption {
-    type = lib.types.attrsOf (lib.types.submodule ({ name, ... }:
-      let
-        dom = lib.lists.findFirst (x: lib.strings.hasSuffix x name) null
-          config.networking.domains;
-      in {
-        config = lib.mkIf (dom != null) {
-          acmeRoot = null;
-          useACMEHost = dom;
-        };
-      }));
-  };
   config = {
-    users.groups.acme.members = [ config.services.nginx.user ];
-    # users.users."${config.services.nginx.user}".extra
-    systemd.services."acme-fixperms".wants = [ "bind.service" ];
-    systemd.services."acme-fixperms".after = [ "bind.service" ];
+    users.groups.acme.members = [config.services.nginx.user];
+    # # users.users."${config.services.nginx.user}".extra
+    systemd.services."acme-fixperms".wants = ["bind.service"];
+    systemd.services."acme-fixperms".after = ["bind.service"];
+    #
+    security.acme = {
+      acceptTerms = true;
+      defaults = {
+        dnsProvider = provider;
 
+        dnsResolver =
+          if dnsResorvers ? "${provider}"
+          then dnsResorvers."${provider}"
+          else throw "${provider} not supported";
+
+      };
+      certs = lib.pipe config.clan.services.ssh-share.acme.subdirs [
+        (map (u: {
+          name = u;
+          value = {
+            domain = "${u}";
+            extraDomainNames = ["*.${u}"];
+            dnsPropagationCheck = true;
+          };
+        }))
+        lib.listToAttrs
+      ];
+    };
+    #
     clan.core = {
       vars.generators.acme = {
-
-        files = { provider = { secret = false; }; };
+        files = {provider = {secret = false;};};
         prompts = {
-
           "prov" = {
             type = "line";
             description = "Set provider (${
-                lib.strings.concatStringsSep " " (lib.attrNames dnsResorvers)
-              })";
+              lib.strings.concatStringsSep " " (lib.attrNames dnsResorvers)
+            })";
           };
           "acme.env" = {
             type = "multiline";
@@ -45,31 +54,6 @@ in {
         '';
       };
     };
-
-    security.acme = {
-      acceptTerms = true;
-      defaults = {
-        dnsProvider = provider;
-
-        dnsResolver = if dnsResorvers ? "${provider}" then
-          dnsResorvers."${provider}"
-        else
-          throw "${provider} not supported";
-
-        environmentFile =
-          config.clan.core.vars.generators.acme.files."acme.env".path;
-      };
-      certs = lib.pipe (config.networking.domains) [
-        (map (u: {
-          name = u;
-          value = {
-            domain = "${u}";
-            extraDomainNames = [ "*.${u}" ];
-            dnsPropagationCheck = true;
-          };
-        }))
-        lib.listToAttrs
-      ];
-    };
+    clan.nginx.acme.email = "contact@stanley-dev.net";
   };
 }
