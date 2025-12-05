63911d57fd5e0968bde33cd3b3336a63c8d2c81b
steryy <contact@steryy.xyz>
2025-12-05 11:57:40 +0100

    refactor: clan service lldap

--- modules/clan/services/lldap/default.nix
+++ modules/clan/services/lldap/default.nix
@@ -1,12 +1,63 @@
-{
+{lib, ...}: let
+  genSecr = pkgs: files: {
+    generators.lldap-passwords = {
+      share = true;
+      inherit files;
+      runtimeInputs = [
+        pkgs.openssl
+      ];
+      script =
+        lib.concatMapStringsSep "\n"
+        (x:
+          #bash
+          ''openssl rand -base64 32 | tr -d '\n' | tr -d ' ' >  "$out/${x}" '') (lib.attrNames files);
+    };
+  };
+in {
   _class = "clan.service";
   manifest.name = "@local/lldap";
   imports = [
     ./server
-    ./server/lldap.nix
     ./client.nix
   ];
   roles.client = {
     description = "Client for lldap server. It also allows for creating shared credentials with server.";
+
+    perInstance = {
+      settings,
+      roles,
+      machine,
+      ...
+    }: let
+      names = lib.attrNames roles.server.machines;
+    in {
+      nixosModule = {pkgs, ...}: {
+        config.clan.core.vars = lib.mkIf (! lib.elem machine.name names) (genSecr pkgs (
+          lib.mapAttrs (_: _: {}) settings.ensureUsers
+        ));
+      };
+    };
+  };
+  roles.server = {
+    perInstance = {roles, ...}: let
+      ensure = lib.pipe roles.client.machines [
+        lib.attrValues
+        (map (x: (x.settings).ensureUsers))
+        (map (
+          lib.mapAttrsToList (n: v: {
+            name = n;
+            value = {};
+          })
+        ))
+        lib.flatten
+        lib.listToAttrs
+      ];
+    in {
+      nixosModule = {pkgs, ...}: {
+        clan.core.vars = genSecr pkgs (
+          lib.mapAttrs (_: _: {}) ensure
+        );
+      };
+    };
   };
 }
