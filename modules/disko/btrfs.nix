0da7279f1cab4071ffd0dbf1030fa9e695a53b68
steryy <contact@steryy.xyz>
2026-01-02 15:05:07 +0100

    feat: add btrfs

---
+++ modules/disko/btrfs.nix
@@ -0,0 +1,51 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        # device = "/dev/vdb";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1024M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountOptions = ["compress=zstd"];
+                    mountpoint = "/";
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  # Parent is not mounted so the mountpoint must be set
+                  "/nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/nix";
+                  };
+                  # Subvolume for the swapfile
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
